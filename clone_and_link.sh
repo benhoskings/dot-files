f5b9bc2bbdf7f3dfdf9ece90133d337cb733a3f9
Ben Hoskings <ben@hoskings.net>
2008-09-16 00:09:46 +1000

    Updated clone_and_link.sh.

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -1,5 +1,9 @@
 git clone git://github.com/benhoskings/dot-files.git ~/.dot-files &&
 cd &&
-ls -1d .dot-files/.??* | while read f; do
+ls -1d .dot-files/.* | while read f; do
+  [ "$f" == '.dot-files/.' ] ||
+  [ "$f" == '.dot-files/..' ] ||
+  [ "$f" == '.dot-files/.git' ] ||
+  echo "Linking $(basename "$f")" &&
   ln -s "$f" .
 done
