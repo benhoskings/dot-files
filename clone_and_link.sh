4e52aff4e3406ad7459aae4ef0be9a1b9c80f8ba
Ben Hoskings <ben@hoskings.net>
2008-11-28 16:53:27 +1000

    No longer specify color=none in clone_and_link.sh.

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -1,6 +1,6 @@
 cd &&
 [ -d '.dot-files' ] || git clone git://github.com/benhoskings/dot-files.git .dot-files &&
-ls -1d --color=none .dot-files/.* | while read f; do
+ls -1d .dot-files/.* | while read f; do
   [ "$f" == '.dot-files/.' ] ||
   [ "$f" == '.dot-files/..' ] ||
   [ "$f" == '.dot-files/.git' ] ||
