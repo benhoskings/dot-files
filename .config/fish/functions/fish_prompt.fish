cbe696ac039d349c9ae970d1a19f035f80af82a1
Ben Hoskings <ben@hoskings.net>
2009-05-25 11:46:13 +1000

    Updated fish_prompt and fish_dirty_files_count.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,5 +1,10 @@
 function fish_prompt --description 'Write out the prompt'
-	printf "%s %s@%s %s%s > " (date '+%a %H:%M:%S') (whoami) (hostname|cut -d . -f 1) (prompt_pwd) (git_cwd_info)
+	set pr_timestamp (date '+%a %H:%M:%S')
+	set pr_user (whoami)
+	set pr_host (hostname | cut -d . -f 1)
+	set pr_cwd (prompt_pwd)
+	set pr_git_info (git_cwd_info)
+	echo -e "\033[90m$pr_timestamp\033[0m $pr_user\033[90m@\033[0m$pr_host \033[90m$pr_cwd$pr_git_info\033[0m \033[32m>\033[0m "
 
 
 end
--- .config/fish/functions/git_dirty_files_count.fish
+++ .config/fish/functions/git_dirty_files_count.fish
@@ -1,6 +1,6 @@
 function git_dirty_files_count
 	if test -d .git
-		git diff --stat | 		tail -n1 | 		sed -E 's/^ *//' | 		sed -E 's/ [a-z ]+\((.)(\),)?/\1/g' | 		sed -E 's/[a-z ]+, / dirty (/'
+		git diff --stat | 		tail -n1 | 		sed -E 's/^ *//' | 		sed -E 's/ [a-z ]+\((.)(\),)?/\1/g' | 		sed -E 's/[a-z ]+, / (/' | 		sed -E "s/([0-9]+)\+/$pr_green\1$pr_grey+/g" | 		sed -E "s/([0-9]+)\-/$pr_red\1$pr_grey-/g"
 	end
 
 
