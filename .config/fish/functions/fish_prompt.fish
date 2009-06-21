a81fbea2f8f8132a8101d51e54a0a080ae6585ac
Ben Hoskings <ben@hoskings.net>
2009-06-21 18:06:20 +1000

    Tidied cwd / git info in fish prompt.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,9 +2,10 @@ function fish_prompt --description 'Write out the prompt'
 	set pr_timestamp (date '+%a %H:%M:%S')
 	set pr_user (whoami)
 	set pr_host (hostname | cut -d . -f 1)
+	set pr_term (test $TERM = 'screen'; and echo "[screen/$WINDOW] ")
 	set pr_cwd (prompt_pwd)
 	set pr_git_info (git_cwd_info)
-	echo -e "\033[90m$pr_timestamp\033[0m $pr_user\033[90m@\033[0m$pr_host \033[90m$pr_cwd$pr_git_info\033[0m \033[32m>\033[0m "
+	echo -e "\033[90m$pr_timestamp\033[0m $pr_user\033[90m@\033[0m$pr_host $pr_cwd\033[90m$pr_git_info\033[0m \033[32m>\033[0m "
 
 
 end
--- .config/fish/functions/git_cwd_info.fish
+++ .config/fish/functions/git_cwd_info.fish
@@ -1,13 +1,12 @@
 function git_cwd_info
 	if test -d .git
-		printf -- " - %s, " (git_parse_branch)
+		printf -- "@%s " (git_parse_branch)
 		if test (git ls-files -m | wc -l) -eq 0
-			echo 'clean'
+			echo '√'
 		else
 			printf "%s" (git_dirty_files_count)
 		end
 	end
-
 
 
 end
