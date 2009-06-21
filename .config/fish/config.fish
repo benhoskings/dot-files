b66dc06cf474178f01d815f6efc02a8e5b9230ca
Ben Hoskings <ben@hoskings.net>
2009-06-21 19:11:25 +1000

    Made 'vi' fish alias OS X-only.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,10 @@ set PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/X11R6/bin
 if test (uname -s) = "Darwin"
   set PATH /opt/local/lib/postgresql83/bin /opt/local/bin $PATH
 
+  function vi
+    mate $argv
+  end
+
   function sed
     gsed $argv
   end
--- .config/fish/functions/vi.fish
+++
@@ -1,3 +0,0 @@
-function vi
-	mate $argv; 
-end
