bd5481254e256148f2801e848fc124d8b455272a
Ben Hoskings <ben@hoskings.net>
2009-06-21 18:50:22 +1000

    Updated git_dirty_files_count to use gsed alias instead of BSD sed.

--- .config/fish/functions/git_dirty_files_count.fish
+++ .config/fish/functions/git_dirty_files_count.fish
@@ -1,6 +1,6 @@
 function git_dirty_files_count
 	if test -d .git
-		git diff --stat | 		tail -n1 | 		sed -E 's/^ *//' | 		sed -E 's/ [a-z ]+\((.)(\),)?/\1/g' | 		sed -E 's/[a-z ]+, / (/' | 		sed -E "s/([0-9]+)\+/$pr_green\1$pr_grey+/g" | 		sed -E "s/([0-9]+)\-/$pr_red\1$pr_grey-/g"
+		git diff --stat | 		tail -n1 | 		sed 's/^ *//' | 		sed -r 's/ [a-z ]+\((.)(\),)?/\1/g' | 		sed -r 's/[a-z ]+, / (/' | 		sed -r "s/([0-9]+)\+/$pr_green\1$pr_grey+/g" | 		sed -r "s/([0-9]+)\-/$pr_red\1$pr_grey-/g"
 	end
 
 
