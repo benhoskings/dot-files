6eada54125e42f10e73b13940f153844ecd735bb
Ben Hoskings <ben@hoskings.net>
2009-06-21 19:00:37 +1000

    Only set OS X-specific paths on OS X.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,11 @@
 
 cd ^/dev/null
 
-set PATH /opt/local/lib/postgresql83/bin /opt/local/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/X11R6/bin
+set PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/X11R6/bin
 
 if test (uname -s) = "Darwin"
+  set PATH /opt/local/lib/postgresql83/bin /opt/local/bin $PATH
+
   function sed
     gsed $argv
   end
