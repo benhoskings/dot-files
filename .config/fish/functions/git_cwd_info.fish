ea7eafed8b44f6e6f04f92f59bd7a12aeaf63e1d
Ben Hoskings <ben@hoskings.net>
2009-06-21 18:19:13 +1000

    Added HEAD's commit id to git fish prompts.

--- .config/fish/functions/git_cwd_info.fish
+++ .config/fish/functions/git_cwd_info.fish
@@ -1,9 +1,7 @@
 function git_cwd_info
 	if test -d .git
-		printf -- "@%s " (git_parse_branch)
-		if test (git ls-files -m | wc -l) -eq 0
-			echo '√'
-		else
+		echo -e ":"(git_parse_branch)"\033[0m@\033[90m"(git_head_commit_id)
+		if not test (git ls-files -m | wc -l) -eq 0
 			printf "%s" (git_dirty_files_count)
 		end
 	end
---
+++ .config/fish/functions/git_head_commit_id.fish
@@ -0,0 +1,5 @@
+function git_head_commit_id
+	git rev-parse --short HEAD
+
+
+end
