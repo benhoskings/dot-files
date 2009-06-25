2ac588f89c19818901bd84c8c3acee52fc566906
Ben Hoskings <ben@hoskings.net>
2009-06-26 02:11:23 +1000

    Removed git_dirty_files_count from prompt, since it sometimes breaks it.

--- .config/fish/functions/git_cwd_info.fish
+++ .config/fish/functions/git_cwd_info.fish
@@ -2,7 +2,7 @@ function git_cwd_info
 	if test -d .git
 		echo -e ":"(git_parse_branch)"\033[0m@\033[90m"(git_head_commit_id)
 		if not test (git ls-files -m | wc -l) -eq 0
-			printf "%s" (git_dirty_files_count)
+			echo "+"
 		end
 	end
 
