c67182c25f7c90ddf3cab26c19b9ba722fd19e07
Ben Hoskings <ben@hoskings.net>
2009-06-30 20:34:05 +1000

    Added git_concise_log_format to config.fish.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,8 @@ cd ^/dev/null
 
 set PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/X11R6/bin
 
+set git_concise_log_format '--pretty=format:%Cblue%h%Creset %cr %Cgreen%an%Creset %s'
+
 if test (uname -s) = "Darwin"
   set PATH /opt/local/lib/postgresql83/bin /opt/local/bin $PATH
 
