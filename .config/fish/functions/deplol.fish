f084d8937fe5e9596c7ad4c86a60809636e84857
Ben Hoskings <ben@hoskings.net>
2009-08-20 14:54:22 +1000

    deplol: no longer stash, and pass args to cap deploy.

--- .config/fish/functions/deplol.fish
+++ .config/fish/functions/deplol.fish
@@ -1,16 +1,14 @@
 function deplol
-	gitst
-	and gitco deploy
+	gitco deploy
 	and git merge work
 	and gitlcg -25
 	and gitco work
-	and gitstp
 	and gitlc origin/deploy..deploy
 	and echo "Hit enter if that looks OK."
 	and read
 	and echo "Pushing..."
 	and git push
-	and cap deploy:all
+	and cap deploy:all $argv
 
 
 end
