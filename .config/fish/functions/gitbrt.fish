0753167e8ef3920fe3db2d0f24f890e4cbba065a
Ben Hoskings <ben@hoskings.net>
2009-08-20 14:55:08 +1000

    gitbrt: separate revisions from paths with --.

--- .config/fish/functions/gitbrt.fish
+++ .config/fish/functions/gitbrt.fish
@@ -1,6 +1,6 @@
 function gitbrt
 	for k in (git branch|perl -pe 's/^..//')
-		echo -e (git show --pretty=format:"%ci (%cr) %Cblue%h%Creset %Cgreen%an%Creset" $k | head -n 1)\\t$k
+		echo -e (git show --pretty=format:"%ci (%cr) %Cblue%h%Creset %Cgreen%an%Creset" $k -- | head -n 1)\\t$k
 	end | 	sort -r
 
 end
