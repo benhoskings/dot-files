fbad12f8960bd0459e25ca281a9ccea2334db7f0
Ben Hoskings <ben@hoskings.net>
2009-08-20 15:03:03 +1000

    Use in_git_repo() to check whether to render git info in the prompt.

--- .config/fish/functions/git_cwd_info.fish
+++ .config/fish/functions/git_cwd_info.fish
@@ -1,5 +1,5 @@
 function git_cwd_info
-	if test -d .git
+	if in_git_repo
 		echo -e ":"(git_parse_branch)"\001\033[0m\002@\033[90m"(git_head_commit_id)
 		if not test (git ls-files -m | wc -l) -eq 0
 			echo "+"
