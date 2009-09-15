a00aeab280917fc054cb648705d022e46410ec22
Ben Hoskings <ben@hoskings.net>
2009-09-15 17:50:13 +1000

    Pass gitbrt args to git branch.

--- .config/fish/functions/gitbrt.fish
+++ .config/fish/functions/gitbrt.fish
@@ -1,6 +1,7 @@
 function gitbrt
-	for k in (git branch|perl -pe 's/^..//')
+	for k in (git branch $argv|perl -pe 's/^..//')
 		echo -e (git show --pretty=format:"%ci (%cr) %Cblue%h%Creset %Cgreen%an%Creset" $k -- | head -n 1)\\t$k
 	end | 	sort -r
+
 
 end
