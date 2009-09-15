4ca85d858e7e24ecf25b5dca8f049646d337dfe9
Ben Hoskings <ben@hoskings.net>
2009-09-15 17:51:02 +1000

    Don't consider ~/.git to be a git repo, since it will be massive.

--- .config/fish/functions/in_git_repo.fish
+++ .config/fish/functions/in_git_repo.fish
@@ -1,8 +1,8 @@
 function in_git_repo
 	set -l g (git rev-parse --git-dir ^/dev/null)
-	if [ -n "$g" ]
-		# we are in git repo
-	end
+	[ -n "$g" ]
+	and [ (pwd) != "~" ]
+	and [ "$g" != "$HOME"/.git ]
 
 
 end
