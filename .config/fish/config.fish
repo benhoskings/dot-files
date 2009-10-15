4177afac51b17b7d3766137e4cb4e0ae9e0581c4
Ben Hoskings <ben@hoskings.net>
2009-10-16 01:01:27 +1100

    Removed Macports from [MAN]PATH.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,8 @@ cd ^/dev/null
 set git_concise_log_format '--pretty=format:%Cblue%h%Creset %cr %Cgreen%an%Creset %s'
 
 if test (uname -s) = "Darwin"
-  set -x PATH /usr/X11/bin /opt/local/lib/postgresql83/bin /opt/local/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/X11R6/bin
-  set -x MANPATH /opt/local/man /usr/X11/man /usr/local/share/man /usr/share/man
+  set -x PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/X11/bin
+  set -x MANPATH /usr/local/share/man /usr/X11/man /usr/share/man
 
   function vi
     mate $argv
