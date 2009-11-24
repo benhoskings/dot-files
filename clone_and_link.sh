301f4796cebe4caac511226041ea89feb73bbe68
Ben Hoskings <ben@hoskings.net>
2009-11-24 16:56:43 +1100

    Moved all the linkable files into files/.

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -2,9 +2,9 @@
 
 cd &&
 [ -d '.dot-files' ] || git clone git://github.com/benhoskings/dot-files.git .dot-files &&
-ls -1d .dot-files/.* | while read f; do
-  [ "$f" == '.dot-files/.' ] ||
-  [ "$f" == '.dot-files/..' ] ||
-  [ "$f" == '.dot-files/.git' ] ||
+ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
+  [ "$f" == '.dot-files/files/.' ] ||
+  [ "$f" == '.dot-files/files/..' ] ||
+  [ "$f" == '.dot-files/files/.git' ] ||
   ln -vsf "$f" .
 done
