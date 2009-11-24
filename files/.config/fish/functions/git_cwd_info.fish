57934a484b7b25c3c303c58c5093cfc9c04723e7
Ben Hoskings <ben@hoskings.net>
2009-11-25 00:10:51 +1100

    Changed ':' to ' ' between path and branch name in git_cwd_info.

--- files/.config/fish/functions/git_cwd_info.fish
+++ files/.config/fish/functions/git_cwd_info.fish
@@ -1,6 +1,6 @@
 function git_cwd_info
 	if in_git_repo
-		echo -e ":"(git_parse_branch)"\001\033[0m\002@\033[90m"(git_head_commit_id)
+		echo -e " "(git_parse_branch)"\001\033[0m\002@\033[90m"(git_head_commit_id)
 		if not test (git ls-files -m | wc -l) -eq 0
 			echo "+"
 		end
