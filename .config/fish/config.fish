1ecc81b8acacc291dba8cb60ae07ff72cd896861
Ben Hoskings <ben@hoskings.net>
2009-11-24 15:01:13 +1100

    Added $gems var, and /usr/local/sbin to fish path.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,14 @@
 
 cd ^/dev/null
 
-set git_concise_log_format '--pretty=format:%Cblue%h%Creset %cr %Cgreen%an%Creset %s'
-
 if test (uname -s) = "Darwin"
-  set -x PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/X11/bin
+  set -x PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin /usr/X11/bin
   set -x MANPATH /usr/local/share/man /usr/X11/man /usr/share/man
 
   function vi
     mate $argv
   end
 end
+
+set gems (gem env gemdir)/gems
+set git_concise_log_format '--pretty=format:%Cblue%h%Creset %cr %Cgreen%an%Creset %s'
