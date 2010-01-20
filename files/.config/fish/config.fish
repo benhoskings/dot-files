1b5088011600fb374240fc53c937f37f83887e16
Ben Hoskings <ben@hoskings.net>
2010-01-20 13:42:55 +1100

    Added ~/bin and /opt/local/bin to the PATH.

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -1,7 +1,7 @@
 cd >/dev/null 2>/dev/null
 
 if test (uname -s) = "Darwin"
-  set -x PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin /usr/X11/bin
+  set -x PATH ~/bin /opt/local/bin /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin /usr/X11/bin
   set -x MANPATH /usr/local/share/man /usr/X11/man /usr/share/man
 
   function vi
