f17ebcbbae86d44457661b0a59e7302760587d67
Jamie Cook <jamie.cook@veitchlister.com.au>
2013-02-09 16:35:08 +1000

    clean up

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -1,12 +1,7 @@
 #!/bin/bash 
 cd &&
-<<<<<<< HEAD
-[ -d '.dot-files' ] || git clone git://github.com/tomjadams/dot-files.git .dot-files &&
-ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
-=======
 [ -d '.dot-files' ] || git clone git://github.com/vlc/dot-files.git .dot-files &&
 ls -1d --color=none .dot-files/files/* .dot-files/files/.* | while read f; do
->>>>>>> 56f76cd8362457dd734519292e13614d63a79244
   [ "$f" == '.dot-files/files/.' ] ||
   [ "$f" == '.dot-files/files/..' ] ||
   [ "$f" == '.dot-files/files/.git' ] ||
