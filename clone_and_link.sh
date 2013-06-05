78749ac90ffb2162687f48734fa7c9e7a9841a21
Ben Hoskings <ben@hoskings.net>
2013-06-05 15:26:00 +1000

    Rewrite clone_and_link.sh from sj26/home to use pushd/popd and update existing repos.

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-cd &&
-[ -d '.dot-files' ] || git clone git://github.com/benhoskings/dot-files.git .dot-files &&
-ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
-  [ "$f" == '.dot-files/files/.' ] ||
-  [ "$f" == '.dot-files/files/..' ] ||
-  [ "$f" == '.dot-files/files/.git' ] ||
-  ln -vsf "$f" .
-done
+pushd "$HOME"
+
+  if [ -d ".dot-files" ]; then
+    pushd ".dot-files"
+      git fetch origin
+      git reset --hard origin/master
+    popd
+  else
+    git clone "git://github.com/benhoskings/dot-files.git" ".dot-files"
+  fi
+
+  ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
+    [ "$f" == '.dot-files/files/.' ] ||
+    [ "$f" == '.dot-files/files/..' ] ||
+    [ "$f" == '.dot-files/files/.git' ] ||
+    ln -vsf "$f" .
+  done
+
+popd
