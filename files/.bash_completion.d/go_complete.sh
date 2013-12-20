18978605d112b77ba6fde2aaf0068cacdea67789
Jamie Cook <jamie.cook@veitchlister.com.au>
2013-12-20 11:42:54 +1000

    awesome auto-complete for the go tool

---
+++ files/.bash_completion.d/go_complete.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+GO_SEARCH_DIRS="$HOME/Projects $HOME/src /opt"
+export GO_SEARCH_DIRS
+
+get_dir_list() {
+  for DIR in $GO_SEARCH_DIRS; do
+    ls -l $DIR | grep ^d | awk '{print $9}'
+  done
+}
+
+find_sub_dirs_strict() {
+  MATCHING_DIR=`ls -l $DIR | grep ^d | awk '{ print $9}' | grep "^$PATTERN" | head -n 1`
+}
+
+find_sub_dirs() {
+  MATCHING_DIR=`ls -l $DIR | grep ^d | awk '{ print $9}' | grep $PATTERN | head -n 1`
+}
+
+go () {
+  PATTERN=$1
+  for DIR in $GO_SEARCH_DIRS; do
+    find_sub_dirs_strict
+    [ -n "$MATCHING_DIR" ] && cd ${DIR}/${MATCHING_DIR} && return
+  done
+
+  for DIR in $GO_SEARCH_DIRS; do
+    find_sub_dirs
+    [ -n "$MATCHING_DIR" ] && cd ${DIR}/${MATCHING_DIR} && return
+  done
+}
+
+_check_go_dirs() {
+  cache_complete /bin/bash .cache_godir "get_dir_list"
+}
+
+complete -F _check_go_dirs -o default go
