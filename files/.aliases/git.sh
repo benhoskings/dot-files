43bc522dae9d9968e35b081d9913faca066b53f4
Jamie Cook <jamie.cook@veitchlister.com.au>
2017-01-10 11:28:38 +1000

    useful git aliases

--- files/.aliases/git.sh
+++ files/.aliases/git.sh
@@ -4,10 +4,12 @@ alias gl='git l'
 alias gp='git push'
 alias ga='git add'
 alias gaa='git add .'
+alias gau='git add -u'
 alias gc='git commit -v'
 alias gb='git branch -v -v'
 alias gba='gb -a'
 alias gc.="gc -m '.'"
+alias gcm="gc -m "
 alias gfo="git fetch origin --prune"
 
 # These are awesome. Only page when necessary.
