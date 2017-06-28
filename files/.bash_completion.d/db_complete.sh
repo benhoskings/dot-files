d8198099f50b2d988da71608616ebc8a2fbfcb68
Jamie Cook <jamie.cook@veitchlister.com.au>
2017-06-28 11:19:38 +1000

    Make db connection a bit smarter

--- files/.bash_completion.d/db_complete.sh
+++ files/.bash_completion.d/db_complete.sh
@@ -4,17 +4,31 @@ LS_COMMAND="ls -l" # or "ls -l --color=auto"
 
 export GO_SEARCH_DIRS
 
+# See https://stackoverflow.com/questions/1494178/how-to-define-hash-tables-in-bash
+declare -A databases
+databases=( ["move_20170606"]="psql -U sa -h move.ctofhnqqey87.us-east-1.rds.amazonaws.com -p 5432 move_20170606" \
+            ["ams-prep"]="psql -U jetski_prep_user -h jetski.ctofhnqqey87.us-east-1.rds.amazonaws.com -p 5432 jetski_prep" \
+            ["ams-snapshot"]="psql -U jetski_prod_user -h jetski-snapshot-jul15.ctofhnqqey87.us-east-1.rds.amazonaws.com -p 5432 jetski_prod" \
+          )
+# echo 
+
 get_db_list() {
-    echo "move_20170606"
+    for i in "${!databases[@]}"; do
+        echo $i
+    done | tr "\n" ' '
 }
 
 db() {
     PATTERN=$1
-    case $PATTERN in
-        move_20170606)
-            psql -U sa -h move.ctofhnqqey87.us-east-1.rds.amazonaws.com -p 5432 move_20170606
-            ;;
-    esac
+    COMMAND=$2
+    CONNECT_STR=${databases[$PATTERN]}
+    if [[ -z "${CONNECT_STR}" ]]; then echo "Can't find database ${PATTERN}"; exit -1; fi
+    if [[ "$COMMAND" == "connect" ]]; then
+        `${CONNECT_STR}`
+    else
+        echo ${COMMAND}
+        echo ${CONNECT_STR}
+    fi
 }
 
 _check_db_dirs() {
